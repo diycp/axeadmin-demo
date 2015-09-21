85f82703e01380542c83b083041db86e5e18212a
李虎头 <kphcdr@163.com>
2015-09-21 11:25:50 +0800

    在线演示

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
 	'DB_TYPE'   => 'mysqli', // 数据库类型
-	'DB_HOST'   => '127.0.0.1', // 服务器地址
-	'DB_NAME'   => 'axe', // 数据库名
-	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => 'root', // 密码
+	'DB_HOST'   => '10.9.1.188', // 服务器地址
+	'DB_NAME'   => 'cf_fab00e37_e7db_4661_bfa0_2ab4b437f65f', // 数据库名
+	'DB_USER'   => 'MMV0bMC968FWRFZD', // 用户名
+	'DB_PWD'    => 'IbkV4n3C6299yhF6', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => 'axe_', // 数据库表前缀
 	'DB_CHARSET'=> 'utf8', // 字符集
